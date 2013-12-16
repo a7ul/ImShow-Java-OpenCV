f8bb4f1eea34352ea6fc72be7a5368225f4c6f52
Atul R <atulanand94@gmail.com>
2013-12-17 01:56:22 +0530

    Finished Fixing Bugs

---
+++ ImShow_JCV/src/com/atul/JavaOpenCV/Imshow.java
@@ -0,0 +1,83 @@
+package com.atul.JavaOpenCV;
+
+/*
+ * Author: ATUL
+ * This code can be used as an alternative to imshow of OpenCV for JAVA-OpenCv 
+ * Make sure OpenCV Java is in your Build Path
+ * Usage :
+ * -------
+ * Imshow ims = new Imshow("Title");
+ * ims.showImage(Mat image);
+ * Check Example for usage with Webcam Live Video Feed
+ */
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
+public class Imshow {
+
+	public JFrame Window;
+	private ImageIcon image;
+	private JLabel label;
+	private MatOfByte matOfByte;
+	private Boolean SizeCustom;
+	private int Height, Width;
+
+	public Imshow(String title) {
+		Window = new JFrame();
+		image = new ImageIcon();
+		label = new JLabel();
+		matOfByte = new MatOfByte();
+		label.setIcon(image);
+		Window.getContentPane().add(label);
+		Window.setResizable(false);
+		Window.setTitle(title);
+		SizeCustom = false;
+	}
+
+	public Imshow(String title, int height, int width) {
+		SizeCustom = true;
+		Height = height;
+		Width = width;
+
+		Window = new JFrame();
+		image = new ImageIcon();
+		label = new JLabel();
+		matOfByte = new MatOfByte();
+		label.setIcon(image);
+		Window.getContentPane().add(label);
+		Window.setResizable(false);
+		Window.setTitle(title);
+
+	}
+
+	public void showImage(Mat img) {
+		if (SizeCustom) {
+			Imgproc.resize(img, img, new Size(Height, Width));
+		}
+		Highgui.imencode(".jpg", img, matOfByte);
+		byte[] byteArray = matOfByte.toArray();
+		BufferedImage bufImage = null;
+		try {
+			InputStream in = new ByteArrayInputStream(byteArray);
+			bufImage = ImageIO.read(in);
+			image.setImage(bufImage);
+			Window.pack();
+			label.updateUI();
+			Window.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ ImShow_JCV/src/examples/example.java
@@ -0,0 +1,54 @@
+package examples;
+
+import com.atul.JavaOpenCV.Imshow;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.VideoCapture;
+
+public class example {
+
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		/*
+		 * Declare a New Frame Here -------------------------
+		 */
+		Imshow im = new Imshow("Video Preview");
+		// This create a Window with Title Video Preview and is autoSized to the image it contains
+		// or
+		// new Imshow("Title",int Height,int Width); to set custom height and width 
+		
+		  
+		 
+		/* You can even Customise the ImShow Frame or Window 
+		 * The image is loaded onto a JFrame which is a public member 
+		 * so that anyone could customise it
+		 * Imshow.Window is the JFrame that one could customise
+		 * For example : 
+		 * By default the Window is not Resizable so to make it resizable:
+		 */
+			im.Window.setResizable(true);
+		// -------------------------
+		Mat m = new Mat();
+		VideoCapture vcam = new VideoCapture(0);
+
+		// loop until VideoCamera is Available
+		while (vcam.isOpened() == false)
+			;
+
+		// Bug Fix: Loop until initial image frames are empty
+		while (m.empty()) {
+			vcam.retrieve(m);
+		}
+
+		while (true) {
+			vcam.retrieve(m);
+			/***
+			 * Show the image
+			 */
+			im.showImage(m);
+			/************/
+		}
+	}
+
+}
